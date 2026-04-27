d5e5a360b12ec08312abe3abf52472a734f51549
funnymataleao <mail@denisefremov.com>
2026-04-27 10:12:18 +0100

    Initial Commit

---
+++ Pickly/PicklyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PicklyApp.swift
+//  Pickly
+//
+//  Created by mataleao on 27/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct PicklyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
